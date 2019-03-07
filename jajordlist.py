60a1a1046a4c7aff3d218dc262672ccbd8200a3e
valoulou <valentin.chaigneau@gmail.com>
2019-03-07 10:20:10 -0500

    zero before number

--- jajordlist.py
+++ jajordlist.py
@@ -10,20 +10,23 @@ wordlistfinal = []
 wordlistupperlower = list(map(''.join, product(*(sorted(set((current.upper(), current.lower()))) for current in basicword))))
 
 wordlistupperlowerwithnumber = []
-
 if numberbefore > 0:
+	formatwithzero = "{:0"+str(numberbefore)+"d}"
 	numberbefore = numberbefore*"9"
-
 	for i in range(int(numberbefore)+1):
 		for current in wordlistupperlower:
+			if len(str(i)) < len(str(numberbefore)):
+				wordlistupperlowerwithnumber.append(formatwithzero.format(i)+current)
 			wordlistupperlowerwithnumber.append(str(i)+current)
 
 if numberafter > 0:
 	numberafter = numberafter*"9"
-
+	formatwithzero = "{:0"+str(numberafter)+"d}"
 	for i in range(int(numberafter)+1):
 		for current in wordlistupperlower:
 			wordlistupperlowerwithnumber.append(current+str(i))
+			if len(str(i)) < len(str(numberafter)):
+				wordlistupperlowerwithnumber.append(formatwithzero.format(i)+current)
 
 wordlistupperlowerandchar = []
 
